256994be23d8a13a965e3ee883e252649b4eb14d
EricBorder9x <140583172+EricBorder9x@users.noreply.github.com>
2023-09-21 16:18:28 +0700

    Add files via upload

--- database/migrations/2023_09_20_083842_create_category_table.php
+++ database/migrations/2023_09_20_083842_create_category_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('category', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('name', 200)->unique();
