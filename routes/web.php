cb05b01984e2f3129a385a55879af51d06468277
Quang Anh <pqanh199@gmail.com>
2023-09-25 19:17:43 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
